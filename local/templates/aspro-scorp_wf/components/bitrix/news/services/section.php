eba23071040b5b42362b7f1e01e2bcd7096df44f
Y <yt@webformat.ru>
2022-09-12 20:14:24 +0500

    Отображен блок специалистов раздела по аналогии со специалистами услуги

--- local/templates/aspro-scorp_wf/components/bitrix/news/services/section.php
+++ local/templates/aspro-scorp_wf/components/bitrix/news/services/section.php
@@ -163,3 +163,77 @@ $this->EndViewTarget();
 		$component
 	);?>
 <?endif;?>
+<?// staff links?>
+<?//if(in_array('LINK_STAFF', $arParams['LIST_PROPERTY_CODE'])) {
+  $dbServicesGroupedBySpecialities = \CIblockElement::getList(['PROPERTY_LINK_STAFF_VALUE' => 'ASC'], ['SECTION_ID' => $arSection['ID'], 'INCLUDE_SUBSECTIONS' => true], ['PROPERTY_LINK_STAFF']);
+  $arSpecIds = [];
+  while($res = $dbServicesGroupedBySpecialities->Fetch()) {
+    $arSpecIds[$res['PROPERTY_LINK_STAFF_VALUE']] = $res['PROPERTY_LINK_STAFF_VALUE'];
+  }
+  if(!empty($arSpecIds)) {
+  ?><div class="wraps nomargin wf-specialists">
+    <hr />
+    <h4 class="underline"><?=GetMessage('T_STAFFS') // GetMessage('T_STAFF')?></h4>
+    <?global $arrrFilter; $arrrFilter = array('ID' => $arSpecIds);?>
+    <?$APPLICATION->IncludeComponent("bitrix:news.list", "staff-linked", array(
+      "IBLOCK_TYPE" => "aspro_scorp_content",
+      "IBLOCK_ID" => CCache::$arIBlocks[SITE_ID]["aspro_scorp_content"]["aspro_scorp_staff"][0],
+      "NEWS_COUNT" => "30",
+      "SORT_BY1" => "SORT",
+      "SORT_ORDER1" => "DESC",
+      "SORT_BY2" => "",
+      "SORT_ORDER2" => "ASC",
+      "FILTER_NAME" => "arrrFilter",
+      "FIELD_CODE" => array(
+        0 => "NAME",
+        1 => "PREVIEW_TEXT",
+        2 => "PREVIEW_PICTURE",
+        3 => "",
+      ),
+      "PROPERTY_CODE" => array(
+        0 => "EMAIL",
+        1 => "POST",
+        2 => "PHONE",
+        3 => "",
+      ),
+      "CHECK_DATES" => "Y",
+      "DETAIL_URL" => "",
+      "AJAX_MODE" => "N",
+      "AJAX_OPTION_JUMP" => "N",
+      "AJAX_OPTION_STYLE" => "Y",
+      "AJAX_OPTION_HISTORY" => "N",
+      "CACHE_TYPE" => "A",
+      "CACHE_TIME" => "360000",
+      "CACHE_FILTER" => "Y",
+      "CACHE_GROUPS" => "N",
+      "PREVIEW_TRUNCATE_LEN" => "",
+      "ACTIVE_DATE_FORMAT" => "d.m.Y",
+      "SET_TITLE" => "N",
+      "SET_STATUS_404" => "N",
+      "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+      "ADD_SECTIONS_CHAIN" => "Y",
+      "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+      "PARENT_SECTION" => "",
+      "PARENT_SECTION_CODE" => "",
+      "INCLUDE_SUBSECTIONS" => "Y",
+      "PAGER_TEMPLATE" => "",
+      "DISPLAY_TOP_PAGER" => "N",
+      "DISPLAY_BOTTOM_PAGER" => "Y",
+      "PAGER_TITLE" => "Новости",
+      "PAGER_SHOW_ALWAYS" => "N",
+      "PAGER_DESC_NUMBERING" => "N",
+      "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+      "PAGER_SHOW_ALL" => "N",
+      "VIEW_TYPE" => "table",
+      "SHOW_TABS" => "N",
+      "SHOW_SECTION_PREVIEW_DESCRIPTION" => "N",
+      "IMAGE_POSITION" => "left",
+      "COUNT_IN_LINE" => "3",
+      "AJAX_OPTION_ADDITIONAL" => ""
+      ),
+      false, array("HIDE_ICONS" => "Y")
+    );?>
+  </div><?
+  }
+//}?>
+
